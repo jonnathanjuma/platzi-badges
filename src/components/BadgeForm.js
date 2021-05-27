85e1e6745ffc9e9d6404c09eb686b04f7e2c4306
Jonnathan Juma <jonnathan_321@hotmail.com>
2021-05-27 12:32:02 -0500

    Agregar formulario submit

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class BadgeForm extends React.Component {
+  handleChange = (e) => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    console.log("Button was clicked");
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form was submitted");
+  };
+  render() {
+    return (
+      <div>
+        <h1>new attendanmt</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>First name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+            ></input>
+          </div>
+
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default BadgeForm;
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,6 +4,7 @@ import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
 import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
   render() {
@@ -18,12 +19,16 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col">
               <Badge
-                firstName="Richard"
-                lastName="Kaufman"
-                twitter="sparragus"
+                firstName="Jota"
+                lastName="Juma"
+                twitter="@jjuma"
                 jobTitle="Frontend Engineer"
                 avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
